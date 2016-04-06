00cce48c0acada71bfa0b99fcd4e691c79d9ebc5
John Liljegren <jliljegren@dtec.com>
2016-04-05 22:43:49 -0400

    [MISC] experimenting with using the Issues tab for listing bugs

--- bugzilla.js
+++ bugzilla.js
@@ -82,3 +82,63 @@ Bugzilla.prototype.updateBug = function(bugId, params) {
 		params: [params]
 	});
 }
+
+/**
+ * Gets a promise that will return bug info for bugs using the passed in criteria.
+ * @param {Object} searchCriteria - The search criteria.
+ * @return {Promise} On success, will return the response object from Bugzilla.
+ */
+Bugzilla.prototype.searchBugs = function(searchCriteria) {
+	"use strict";
+
+	return $.xmlrpc({
+		url: this.url,
+		methodName: 'Bug.search',
+		params: [searchCriteria]
+	});
+}
+
+/**
+ * Gets a promise that will return all products.
+ * @return {Promise} On success, will return the response object from Bugzilla.
+ */
+Bugzilla.prototype.getProducts = function() {
+	"use strict";
+	
+	return $.xmlrpc({
+		url: this.url,
+		methodName: 'Product.get_enterable_products'
+	})
+	.then(function(response) {
+		return $.xmlrpc({
+			url: this.url,
+			methodName: 'Product.get',
+			params: [{ids: response[0].ids}],
+			dataFilter: function(data, type) {
+				// this fixes a problem where Bugzilla sends malformed XML
+				return data.replace(/<\/methodR.*/, '</methodResponse>');
+			}
+		});
+	});
+}
+
+/**
+ * Modification to jquery.xmlrpc to handle dates.
+ * See https://github.com/timheap/jquery-xmlrpc/issues/5
+ */
+$.xmlrpc.makeType('dateTime.iso8601', true, function(d) {
+	return [
+		d.getUTCFullYear(), '-', _pad(d.getUTCMonth()+1), '-',
+		_pad(d.getUTCDate()), 'T', _pad(d.getUTCHours()), ':',
+		_pad(d.getUTCMinutes()), ':', _pad(d.getUTCSeconds()), 'Z'
+	].join('');
+}, function(text, node) {
+		// ISO 8601 dates can be either YYYY-MM-DD _or_
+		// YYYYMMDD. Added check for the latter case, since it's
+		// not handled by FireFox's Date constructor. jfuller
+		// 2013-05-13
+		if (!/-/.test(text)) {
+			text = text.replace(/(\d{4})(\d{2})(\d{2})(.+)/, "$1-$2-$3$4");
+		}
+	return new Date(text);
+});
--- content.js
+++ content.js
@@ -1,3 +1,13 @@
+/**
+ * Here, we're setting up a map between Bugzilla product and GitHub repo in the user's storage.
+ */
+var productMap = {};
+chrome.storage.sync.get('productMap', function (obj) {
+	if (obj && obj.productMap) {
+		productMap = obj.productMap;
+	}
+});
+		
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	switch (request.message) {
 		/* This will reinitialize the plugin when the extension button is pressed */
@@ -6,16 +16,21 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			break;
 		/* This will reinitialize the plugin when the tab is updated */
 		case "tab_updated":
-			init();
+			chrome.storage.sync.get('productMap', function (obj) {
+				if (obj && obj.productMap) {
+					productMap = obj.productMap;
+				}
+				init();
+			});
 			break;
 	}
 });
-		
+
 function init() {
 	"use strict";
 	// This object will be used to interact with Bugzilla
 	var bugzilla = new Bugzilla();
-
+	
 	// Check the URL to determine if we're in Bugzilla or GitHub
 	if (location.href.indexOf("bugzilla") > -1) {
 		// We're in Bugzilla
@@ -48,6 +63,8 @@ function init() {
 	}
 	else if (location.href.indexOf("github") > -1) {
 		// We're in GitHub
+		
+		var repo = location.href.replace(/.*.com\//, '').split('/')[1];
 		
 		var REGEX = /^\[(\d+)\]|^(\d+)|^Bug\s*(\d+)/; // for example, matches [83508], 83508, Bug83508 or Bug 83508
 		var $issueTitle = $('.js-issue-title');
@@ -89,7 +106,7 @@ function init() {
 				);
 				
 				/* Put Bugzilla bug info into side section */
-				bugzilla.getBug(bugId).success(function(response) {
+				bugzilla.getBug(bugId).done(function(response) {
 					var bugInfo = response[0].bugs[0];
 					
 					$(".sidebar-dit-bugzilla div")
@@ -186,5 +203,152 @@ function init() {
 					});
 			}
 		}
+		
+		if (repo && repo.length > 0 && !productMap[repo] && $("#productMapSelector").length === 0) {
+			$("body").before(
+				$("<div>")
+					.attr({id: "productMapSelector"})
+					.addClass("commit-tease")
+					.html(
+						$("<img>")
+							.attr({src: chrome.extension.getURL("icon.png")})
+							.css({height: '1.5em', margin: '0 5px', 'vertical-align': 'text-bottom'})
+					)
+					.append("Bugzilla product not set.  Loading products...")
+			);
+			
+			bugzilla.getProducts().done(function(response) {
+				var products = response[0].products.sort(function(a, b) {
+					return (a.name < b.name ? -1 : (a.name > b.name ? 1 : 0));
+				});
+				$("div#productMapSelector")
+					.html(
+						$("<img>")
+							.attr({src: chrome.extension.getURL("icon.png")})
+							.css({height: '1.5em', margin: '0 5px', 'vertical-align': 'text-bottom'})
+					)
+					.append("Please choose the Bugzilla product this repo is associated with:")
+					.append(
+						$("<select>")
+							.addClass("form-control input-sm")
+							.css("margin", "0 5px")
+							.append("<option>")
+							.append(
+								$.map(products, function(el, i) {
+									return $("<option>").val(el.name).html(el.name);
+								})
+							)
+					)
+					.append(
+						$("<button>")
+							.html("OK")
+							.addClass("btn btn-sm btn-primary")
+							.click(function() {
+								var selectedProduct = $(this).prev().val();
+								productMap[repo] = {
+									name: selectedProduct
+								};
+								chrome.storage.sync.set({productMap: productMap}, function(obj) {
+									$("#productMapSelector").remove();
+								});
+							})
+					)
+					.append(
+						$("<button>")
+							.html("Cancel")
+							.addClass("btn btn-sm")
+							.css("margin", "0 5px")
+							.click(function() {
+								$("#productMapSelector").remove();
+							})
+					);
+			});
+		}
+		
+		function syncIssues() {
+			chrome.storage.local.get('bugs', function(obj) {
+				var bugs = obj.bugs[repo] || [];
+				var $spans = $("a[href$='/issues']:not([href^='/issues'])").children("span");
+				var numTotal = bugs.length;
+				if ($spans.eq(0).html() === "Issues") {
+					var numIssues = parseInt($spans.eq(1).html(), 10);
+					if (productMap[repo]) {
+						productMap[repo].numIssues = numIssues;
+					}
+					numTotal += numIssues;
+					$spans.eq(0).html("Issues/Bugs");
+				}
+				else if (productMap[repo]) {
+					numTotal += productMap[repo].numIssues;
+				}
+				
+				$spans.eq(1).html(numTotal);
+			});
+		}
+		
+		syncIssues();
+		
+		$("body")
+			/* Lists bugs from Bugzilla in the Issues section */
+			.off("click.DITBugzillaGitHub", "a[href$='/issues']:not([href^='/issues'])")
+			.on("click.DITBugzillaGitHub", "a[href$='/issues']:not([href^='/issues'])", function() {
+				if (!productMap[repo]) {
+					return;
+				}
+				bugzilla.searchBugs({
+					product: [productMap[repo].name],
+					status: ['NEW','ASSIGNED','REOPENED']
+				})
+				.done(function(response) {
+					var bugs = response[0].bugs;
+					
+					if (bugs.length > 0) {
+						// Make sure the issues list is not empty
+						$("div.blankslate")
+							.hide()
+							.after(
+								$('<ul class="table-list table-list-bordered table-list-issues js-navigation-container js-active-navigation-container">')
+							);
+							
+						// Update the numbers
+						var bugStruct = {};
+						bugStruct[repo] = bugs;
+						chrome.storage.local.set({bugs: bugStruct}, function() {
+							syncIssues();
+						});
+					}
+					
+					var $ul = $("ul.table-list-issues");
+					
+					$.each(response[0].bugs, function(){
+						$ul.append(
+							$('<li class="selectable read table-list-item js-navigation-item js-issue-row">')
+								.append('<label class="table-list-cell table-list-cell-checkbox">')
+								.append('<div class="table-list-cell table-list-cell-type">')
+								.append(
+									$('<div class="table-list-cell issue-title">')
+										.append(
+											$('<a href="' + bugUrl + this.id + '" class="issue-title-link js-navigation-open">')
+												.append('[' + this.id + '] ' + this.summary)
+										)
+										.append(
+											$('<div class="issue-meta">')
+												.append('<span class="issue-meta-section opened-by">Bug reported <time datetime="' + this.creation_time + '" is="relative-time"></time> by ' + this.creator + '</span>')
+												.append(
+													$('<span class="issue-meta-section css-truncate issue-milestone">')
+														.append(
+															$('<a class="milestone-link muted-link css-truncate tooltipped tooltipped-s" aria-label="View all issues in this milestone" href="/ditinc/35B/milestones/' + encodeURIComponent(this.target_milestone) + '">')
+																.append('<svg aria-hidden="true" class="octicon octicon-milestone" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M8 2H6V0h2v2z m4 5H2c-0.55 0-1-0.45-1-1V4c0-0.55 0.45-1 1-1h10l2 2-2 2zM8 4H6v2h2V4z m-2 12h2V8H6v8z"></path></svg>')
+																.append('<span class="css-truncate-target">&nbsp;' + this.target_milestone + '</span>')
+          												)
+          										)
+										)
+								)
+								.append('<div class="table-list-cell table-list-cell-avatar">')
+								.append('<div class="table-list-cell issue-comments">')
+						);
+					});
+				});
+			})
 	}
 }
