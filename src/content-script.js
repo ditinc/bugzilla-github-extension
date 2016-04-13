c1d9bc48d5918318ebb9cb150434ac5678b471ec
John Liljegren <jliljegren@dtec.com>
2016-04-13 17:05:19 -0400

    [MISC] major overhaul: more integrated w/GitHub now! (direct to master)

---
+++ src/content-script.js
@@ -0,0 +1,104 @@
+'use strict';
+
+// Check the URL to determine if we're in Bugzilla or GitHub
+if (location.href.indexOf("bugzilla") > -1) {
+	// We're in Bugzilla
+	
+	var url = $('#cf_pull_request_number').val();
+	
+	if (url && url.length) {
+		var urlArray = url.split('/');
+		var pr = urlArray[urlArray.length - 1];
+		
+		/* This will put the pull request # as a link next to the bug title */
+		if (url) {
+			var $bugTitle = $('#summary_alias_container');
+			
+			if ($($bugTitle[0].previousSibling).is("A")) {
+				$($bugTitle[0].previousSibling).remove();
+			}
+			
+			$bugTitle[0].previousSibling.textContent = " - ";
+			
+			$bugTitle.before(
+				$("<a>")
+					.attr({
+						"href": url
+					})
+					.html('[#' + pr + ']')
+			);
+		}
+	}
+}
+else if (location.href.indexOf("github") > -1) {
+	// This injects the script that requires access to the window object into the DOM.
+	var s = document.createElement('script');
+	s.src = chrome.extension.getURL('src/injected.js');
+	s.onload = function() {
+		this.parentNode.removeChild(this);
+	};
+	(document.head || document.documentElement).appendChild(s);
+	
+	// This object will be used to interact with Bugzilla.
+	var bugzilla = new Bugzilla();
+	
+	// We'll accept messages from the injected script in order to make calls to Bugzilla.
+	window.addEventListener('message', function(event) {
+		// Only accept messages from same frame
+		if (event.source !== window) {
+			return;
+		}
+	
+		var message = event.data;
+	
+		// Only accept messages that we know are ours
+		if (typeof message !== 'object' || message === null || !message.method) {
+			return;
+		}
+	
+		switch (message.method) {
+			/* Puts Bugzilla bug info into our sidebar section */
+			case "loadBugDetails":
+				bugzilla.getBug(message.bugId).done(function(response) {
+					var bugInfo = response[0].bugs[0];
+					
+					$('.sidebar-dit-bugzilla div')
+						.html(
+							$('<p class="reason text-small text-muted">')
+								.html("Status: " + bugInfo.status)
+						)
+						.append(
+							$('<p class="reason text-small text-muted">')
+								.html("Resolution: " + bugInfo.resolution)
+						)
+						.append(
+							$('<p class="reason text-small text-muted">')
+								.html("LOE: " + bugInfo.estimated_time)
+						)
+						.append(
+							$('<p class="reason text-small text-muted">')
+								.html("Charge Code: " + bugInfo.cf_chargecode)
+						)
+						.append(
+							$('<p class="reason text-small text-muted">')
+								.html("Assignee: " + bugInfo.assigned_to)
+						)
+						.append(
+							$('<p class="reason text-small text-muted">')
+								.html("QA Contact: " + bugInfo.qa_contact)
+						);
+				});
+				break;
+			
+			/* Sends comment to Bugzilla */
+			case "addComment":
+				bugzilla.addComment(message.bugId, message.comment, message.hoursWorked);
+				break;
+				
+			/* Updates bug details */
+			case "updateBug":
+				bugzilla.updateBug(message.bugId, message.params);
+				break;
+		}
+	});
+}
---
+++ src/injected.js
@@ -0,0 +1,200 @@
+'use strict';
+
+var DITBugzillaGitHub = function() {
+	var bugUrl = "https://bugzilla.dtec.com/show_bug.cgi?id="; // TODO: make this dynamic
+	var bugId;
+	var $ = require('github/jquery').default;
+	
+	var init = function() {
+		createListeners();
+		linkifyBugNumber(document);
+		showBugDetailsInSidebar(document);
+		injectHoursWorkedInput(document);
+	};
+	
+	var getBugUrl = function() {
+		return bugUrl + bugId;
+	}
+
+	var createListeners = function() {
+		// This handles partial updates
+		var proxied = $.fn.replaceWith;
+		$.fn.replaceWith = function(contents) {
+			linkifyBugNumber(contents);
+			showBugDetailsInSidebar(contents);
+			injectHoursWorkedInput(contents);
+			return proxied.apply(this, arguments);
+		};
+		
+		var pjaxBeforeReplaceHandler = function(e) {
+			linkifyBugNumber(e.originalEvent.detail.contents);
+			showBugDetailsInSidebar(e.originalEvent.detail.contents);
+			injectHoursWorkedInput(e.originalEvent.detail.contents);
+		};
+		
+		$(document)
+			/* Allows us to modify content before GitHub renders it... this handles PJAX */
+			.off("pjax:beforeReplace", pjaxBeforeReplaceHandler)
+			.on("pjax:beforeReplace", pjaxBeforeReplaceHandler)
+			
+			/* Syncs comments with the bug in Bugzilla */
+			.off("click.DITBugzillaGitHub", "#partial-new-comment-form-actions button")
+			.on("click.DITBugzillaGitHub", "#partial-new-comment-form-actions button", function() {
+				var comment = $("#new_comment_field").val();
+				if ($.trim(comment).length) {
+					window.postMessage({method: "addComment", bugId: bugId, comment: comment, hoursWorked: $("#workTime").val()}, '*');
+				}
+			})
+			
+			/* Syncs line comments with the bug in Bugzilla */
+			.off("click.DITBugzillaGitHub", ".js-inline-comment-form button[type='submit']")
+			.on("click.DITBugzillaGitHub", ".js-inline-comment-form button[type='submit']", function() {
+				var $form = $(this).closest("form");
+				var comment = $form.find("textarea").val();
+				var line = $form.find("[name='line']").val();
+				var path = $form.find("[name='path']").val();
+				if ($.trim(comment).length) {
+					if (line) {
+						comment = path + ": " + comment;
+					}
+					else {
+						comment = path + " line " + line + ": " + comment
+					}
+					window.postMessage({method: "addComment", bugId: bugId, comment: comment, hoursWorked: 0}, '*');
+				}
+			})
+				
+			/* Updates the code status in Bugzilla when merging a pull request */
+			.off("click.DITBugzillaGitHub", "button[type='submit'].js-merge-commit-button")
+			.on("click.DITBugzillaGitHub", "button[type='submit'].js-merge-commit-button", function() {
+				var newCodeStatus;
+				var comment = "Merged pull request " + $(".gh-header-number").html();
+				var mergeTarget = $(".current-branch").eq(0).children().html();
+				
+				if (mergeTarget === "master") {
+					newCodeStatus = "Merged to master/trunk";
+					comment += " to master.";
+				}
+				else {
+					newCodeStatus = "Merged to parent branch";
+					comment += " to parent branch.";
+				}
+				
+				window.postMessage({method: "updateBug", bugId: bugId, params: {"cf_codestatus": newCodeStatus, "comment": {"body": comment}}}, '*');
+			});
+	};
+
+	var linkifyBugNumber = function(contents) {
+		var $issueTitle = $(contents).find('.js-issue-title');
+		var newHtml = $issueTitle.html();
+
+		if ($issueTitle.length && $issueTitle.children('a').length === 0) {
+			var REGEX = /^\[(\d+)\]|^(\d+)|^Bug\s*(\d+)/; // for example, matches [83508], 83508, Bug83508 or Bug 83508
+			var matches = $issueTitle.html().match(REGEX);
+			
+			if (matches && matches.length) {
+				bugId = matches[0].match(/\d+/)[0];
+				
+				/* This will turn the bug number into a link to the bug */
+				newHtml = $issueTitle.html().replace(REGEX, '<a href="' + getBugUrl() + '">[' + bugId + ']</a>');
+			}
+		}
+
+		$issueTitle.html(newHtml);
+	};
+	
+	var showBugDetailsInSidebar = function(contents) {
+		var selector = '#partial-discussion-sidebar';
+		var $sidebar;
+		
+		if ($(contents).length === 1 && $(contents).is(selector)) {
+			$sidebar = $(contents);
+		}
+		else {
+			try {
+				$sidebar = $(contents).find(selector);
+			}
+			catch(e) {
+				// I don't know why but sometimes .find() fails if there are nulls
+			}
+		}
+		
+		if ($sidebar.length) {
+			$sidebar.prepend(
+				$("<div>")
+					.addClass("discussion-sidebar-item sidebar-dit-bugzilla")
+					.append(
+						$("<h3>")
+							.addClass("discussion-sidebar-heading")
+							.html("Bugzilla ")
+							.append(
+								$("<a>")
+									.attr("href", getBugUrl())
+									.html("[" + bugId + "]")
+							)
+					)
+					.append(
+						$("<div>")
+							.html(
+								$('<p class="reason text-small text-muted">')
+									.html("Loading...")
+							)
+					)
+			);
+			
+			window.postMessage({method: "loadBugDetails", bugId: bugId}, '*');
+		}
+	};
+	
+	var injectHoursWorkedInput = function(contents) {
+		var selector = '#partial-new-comment-form-actions button';
+		var $buttons;
+		
+		if ($(contents).length === 1 && $(contents).is(selector)) {
+			$buttons = $(contents);
+		}
+		else {
+			try {
+				$buttons = $(contents).find(selector);
+			}
+			catch(e) {
+				// I don't know why but sometimes .find() fails if there are nulls
+			}
+		}
+		
+		if ($buttons.length) {
+			$buttons
+				.last()
+					.after(
+						$("<label>")
+							.text("Hours Worked")
+							.attr({
+								for: "workTime"
+							})
+							.css({
+								float: "right",
+								padding: "7px 0"
+							})
+					)
+					.after(
+						$("<input>")
+							.attr({
+								name: "workTime",
+								id: "workTime",
+								type: "number",
+								step: "0.5"
+							})
+							.css({
+								width: "2.5em",
+								float: "right",
+								margin: "5px"
+							})
+					);
+					
+		}
+	};
+	
+	init();
+};
+
+new DITBugzillaGitHub();
